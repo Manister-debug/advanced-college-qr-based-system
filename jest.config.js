5d338c5fb4fb9148a42e74799a490524d5e65574
Amjad181 <96491045+Amjad181@users.noreply.github.com>
2025-12-24 11:33:48 +0300

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/test/setup/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    'pages/**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  testMatch: [
+    '<rootDir>/test/unit/**/*.test.[jt]s?(x)',
+    '<rootDir>/test/integration/**/*.test.[jt]s?(x)',
+  ],
+};
+
+module.exports = createJestConfig(customJestConfig);
